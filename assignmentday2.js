f29333b94d560e2ca1a2c31ed1cae09541d979fa
kausalyakirankumar <kausalyakirankumar@gmail.com>
2026-04-26 18:07:16 +0530

    Add files via upload

---
+++ assignmentday2.js
@@ -0,0 +1,17 @@
+
+//create a function
+function isOddOrEven(number){ 
+
+    //use conditional statement
+
+    if(number % 2=== 0){
+        console.log(number,":is even number")
+
+    }
+    else{
+         console.log(number,":is odd number")
+
+    }   
+
+}
+  isOddOrEven(7)
