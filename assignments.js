c1d791286b5d2ba676a5638b896045cd2e720ad6
kausalyakirankumar <kausalyakirankumar@gmail.com>
2026-04-26 12:51:18 +0530

    Added my assignment of Day 1

---
+++ assignments.js
@@ -0,0 +1,19 @@
+var firstname="kausalya"
+console.log(firstname)
+console.log(typeof(firstname))
+
+var companyName="Compumatrice"
+console.log(companyName)
+console.log(typeof(companyName))
+
+var phonenumber=8888888888
+console.log(phonenumber)
+console.log(typeof(phonenumber))
+
+var isAutomation=true
+console.log(isAutomation)
+console.log(typeof(isAutomation))
+
+var hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof(hasPlaywright))
